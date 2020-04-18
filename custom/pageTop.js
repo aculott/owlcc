7cc3c4a65e13658aabbf7e6d9094dfb74ae07dd8
aculott <culottaenterprises@gmail.com>
2020-04-18 08:56:03 -0500

    Update pageTop.js

--- custom/pageTop.js
+++ custom/pageTop.js
@@ -1,7 +1,7 @@
 document.write('<div id="topBar">');
 document.write('<table><tr>');
-document.write('<td><a href="/index.html"><img src="custom/companyLogo.jpg" alt="Twilio" style="width:120px; padding-right: 30px;"/></a></td>');
-document.write('<td><h1>Prayer Call Center</h1></td>');
+document.write('<td><a href="/index.html"><img src="custom/companyLogo.JPG" alt="Twilio" style="width:120px; padding-right: 30px;"/></a></td>');
+document.write('<td><h1>Prayer Call Center One on One</h1></td>');
 document.write('</tr></table>');
 document.write('<hr>');
 document.write('</div>');
